492a62631b5baffe80d82c48bbec8eddf9829d10
DefCon-018 <hp761570@gmil.com>
2021-04-09 12:12:19 +0530

    Add animations

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ let body = selectElement('body');
 menuToggler.addEventListener('click', function(e){
     body.classList.toggle('open');
 })
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '90rem',
+    delay: 300
+})
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '90rem',
+    delay: 600
+})
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '90rem',
+    delay: 500
+})
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '90rem',
+    delay: 500
+})
