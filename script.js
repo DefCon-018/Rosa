97321df56457e97abb55abd7626e4c0778bcb4e6
DefCon-018 <hp761570@gmil.com>
2021-06-26 15:18:07 +0530

    last commit

--- script.js
+++ script.js
@@ -14,28 +14,28 @@ window.sr = ScrollReveal();
 
 sr.reveal('.animate-left', {
     origin: 'left',
-    duration: 1000,
+    duration: 500,
     distance: '90rem',
-    delay: 300
+    delay: 100
 })
 
 sr.reveal('.animate-right', {
     origin: 'right',
-    duration: 1000,
+    duration: 500,
     distance: '90rem',
-    delay: 600
+    delay: 200
 })
 
 sr.reveal('.animate-top', {
     origin: 'top',
-    duration: 1000,
+    duration: 500,
     distance: '90rem',
-    delay: 500
+    delay: 200
 })
 
 sr.reveal('.animate-bottom', {
     origin: 'bottom',
-    duration: 1000,
+    duration: 500,
     distance: '90rem',
-    delay: 500
+    delay: 200
 })
